07ac6518c4f638527d9e0e86bdc16e8c0cfb4f74
alireza easazade <easazade@gmail.com>
2023-06-14 22:35:04 +0330

    Add a ContextData for testing in ShopStore

--- lib/stores/shop_store.dart
+++ lib/stores/shop_store.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_crystalline/flutter_crystalline.dart';
 
 class ShopStore extends ChangeNotifierData {
-  late final Data<String> shopName = Data<String>();
+  late final shopName = ContextData<String, int>();
 
   Future setShopName(String name) async {
-    const String what = 'WHATT ??';
+    const String what = 'AliShop';
     shopName.operation = Operation.loading;
     notifyListeners();
 
@@ -12,8 +12,10 @@ class ShopStore extends ChangeNotifierData {
     shopName.operation = Operation('meow');
     notifyListeners();
     await Future.delayed(const Duration(seconds: 2));
+    shopName.context = 12;
     if (shopName.valueEqualsTo(what)) {
-      shopName.value = 'AHA !!';
+      shopName.value =
+          'AliKala !! ${(shopName.hasContext) ? "with ${shopName.context} years of customer service" : ""}';
     } else {
       shopName.value = what;
     }
